2ee5d15e43eef2d5e39044c4a3b97d3fa7be3d68
Alex Fallenstedt and Brad Pizzimenti <a.fallenstedt@gmail.com>
2016-04-05 08:27:11 -0700

    add zip lookup form

--- app/components/lookup-form/component.js
+++ app/components/lookup-form/component.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    submitZip() {
+      var zip = this.get('zip');
+      this.sendAction('zipLookup', zip);
+    }
+  }
 });
